c2776015bb5707fb0cabbf27b41c121f6919e410
James Esguerra <james.a.esguerra@obf.ateneo.edu>
2022-08-18 16:36:22 +0800

    add files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import './App.css';
+import { ExportToCsv } from 'export-to-csv';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [articleData, setArticleData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [category, setCategory] = useState('bansa');
+  const [numOfPages, setNumOfPages] = useState(0);
+
+  const fetchData = async() => {
+    setIsLoading(true);
+    const url = `https://powerful-wave-68939.herokuapp.com/api/articles?category=${category}&pages=${numOfPages}`;
+    const response = await axios.get(url);
+    await setArticleData(response.data);
+    console.log(response.data);
+    setIsLoading(false);
+  }
+
+  const downloadCSV = () => {
+    const options = {
+      fieldSeparator: ',',
+      filename: `PSN-${category}`,
+      quoteStrings: '"',
+      decimalSeparator: '.',
+      showLabels: true, 
+      title: `PSN-${category}`,
+      useTextFile: false,
+      useBom: false,
+      headers: ['title', 'article_text', 'article_date', 'source']
+    }
+
+    const csvExporter = new ExportToCsv(options);
+    csvExporter.generateCsv(articleData);
+  }
+
+  return(
+    <div className='App'>
+      <div>
+        <h1>Pilipino Star Ngayon</h1>
+        {isLoading !== true || <p>fetching...</p>}
+        <h2>Category: <span style={{color: 'red'}}>{category}</span></h2>
+        <button onClick={() => setCategory('bansa')}>Bansa</button>
+        <button onClick={() => setCategory('metro')}>Metro</button>
+        <button onClick={() => setCategory('probinsiya')}>Probinsiya</button>
+        <button onClick={() => setCategory('opinyon')}>Opinyon</button>
+        <button onClick={() => setCategory('palaro')}>Palaro</button>
+      </div>
+      <div>
+        <h2>Number of pages:</h2>
+        <input type='text' value={numOfPages} onChange={(e) => setNumOfPages(e.target.value)} />
+      </div>
+      <div>
+        <button onClick={fetchData}>Fetch data</button>
+        <button onClick={downloadCSV}>Download CSV</button>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
